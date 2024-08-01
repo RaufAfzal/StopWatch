dbc7549d36f842b11bba170fd1f133b22fa978e6
Rauf Afzal <raufafzal453@gmail.com>
2024-08-02 00:32:34 +0500

    StopWatch in React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [randomInput, setRandomInput] = useState('');
+  const [seconds, setSeconds] = useState(0);
+  const timerId = useRef(0);
+  const renders = useRef(0);
+
+
+  const handleUserInput = (e) => {
+    setRandomInput(e.target.value)
+    renders.current++;
+  }
+
+  const startTimer = () => {
+    timerId.current = setInterval(() => {
+      renders.current++;
+      setSeconds(prev => prev + 1)
+    }, 1000)
+  }
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+    timerId.current = 0;
+  }
+
+  const resetTimer = () => {
+    stopTimer()
+    if (seconds)
+      renders.current++;
+    setSeconds(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='App'>
+      <label>Random Input</label>
+      <br></br>
+      <br></br>
+      <input
+        type="text"
+        placeholder='Random Input'
+        value={randomInput}
+        onChange={handleUserInput} />
+      <br></br>
+      <br></br>
+      {randomInput} <br></br>
+      <section>
+      <button onClick={startTimer} >Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer} >Reset</button>
+      </section>
+
+
+      <br></br>
+      <p> Seconds: {seconds}</p>
+      <p> Renders: {renders.current}</p>
     </div>
-  );
+
+  )
 }
 
 export default App;
